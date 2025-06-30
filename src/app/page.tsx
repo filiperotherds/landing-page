ac7b6d43ebb4e9dfbb71051875cffa81ebedb3e1
Filipe <filiperotherds@gmail.com>
2025-06-30 13:05:29 -0300

    first config at lp

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+export default function Home() {
+  return (
+    <div>
+      <header className="h-20 w-full flex items-center px-8">
+        Logo
+      </header>
+      <section className="w-full flex flex-col items-center px-8">
+        <div className="w-full flex items-start justify-start">
+          <p className="text-xl">
+            <span>
+              Clínicas de Reabilitação
+            </span>
+          </p>
+        </div>
+      </section>
+      <section>recursos section</section>
+      <section>unidades section</section>
+      <section>benefícios section</section>
+      <section>FAQ section</section>
+      <footer>footer</footer>
+    </div>
+  );
+}
