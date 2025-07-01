f44243b89cbf9d600ae6d141a96338cefbf783b2
Filipe <filiperotherds@gmail.com>
2025-07-01 13:34:43 -0300

    finish features section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 import Link from "next/link";
 import Image from "next/image";
 
-import { FaceId, HeartSolid, StarSolid, Whatsapp } from "iconoir-react";
+import {
+  DeliveryTruck,
+  Donate,
+  FaceId,
+  HeartSolid,
+  StarSolid,
+  Whatsapp,
+} from "iconoir-react";
 import { ChevronRight } from "lucide-react";
 
 import { Carousel } from "@/components/carousel";
@@ -276,11 +283,11 @@ export default function Home() {
 
       {/* Feature Section */}
       <section className="w-full flex flex-col items-center justify-center bg-[#e3e8ec] p-8 gap-8">
-        <div className="w-full flex flex-col items-center justify-center gap-2">
+        <div className="w-full flex flex-col items-center justify-center gap-3">
           <FaceId
             className="text-zinc-600"
-            width={36}
-            height={36}
+            width={38}
+            height={38}
             strokeWidth={1.5}
           />
 
@@ -307,27 +314,27 @@ export default function Home() {
           </Link>
         </div>
 
-        <div className="w-full flex flex-col items-center justify-center gap-2">
-          <FaceId
+        <div className="w-full flex flex-col items-center justify-center gap-3">
+          <Donate
             className="text-zinc-600"
-            width={36}
-            height={36}
+            width={38}
+            height={38}
             strokeWidth={1.5}
           />
 
           <h1 className="font-sans text-zinc-800 font-semibold">
-            Tratamento Individualizado
+            Apoio Familiar
           </h1>
 
           <p className="text-xs text-zinc-500 text-center">
-            Entendemos que cada pessoa é única. Por isso, o tratamento é 100%
-            focado nas necessidades específicas do paciente, avaliando seu
-            histórico de vida e saúde para criar um plano de recuperação mais
-            eficaz e humano.
+            A dependência química afeta toda a família. Oferecemos sessões de
+            terapia e grupos de apoio para os familiares, fornecendo as
+            ferramentas necessárias para entender o processo, curar as relações
+            e ajudar na prevenção de recaídas.
           </p>
 
           <Link
-            href={"/tratamento-individualizado"}
+            href={"/apoio-familiar"}
             className="font-sans text-xs text-blue-600"
           >
             <div className="flex flex-row items-center justify-center gap-0.5">
@@ -338,23 +345,23 @@ export default function Home() {
           </Link>
         </div>
 
-        <div className="w-full flex flex-col items-center justify-center gap-2">
-          <FaceId
+        <div className="w-full flex flex-col items-center justify-center gap-3">
+          <DeliveryTruck
             className="text-zinc-600"
-            width={36}
-            height={36}
+            width={38}
+            height={38}
             strokeWidth={1.5}
           />
 
           <h1 className="font-sans text-zinc-800 font-semibold">
-            Tratamento Individualizado
+            Suporte para Remoção 24h
           </h1>
 
           <p className="text-xs text-zinc-500 text-center">
-            Entendemos que cada pessoa é única. Por isso, o tratamento é 100%
-            focado nas necessidades específicas do paciente, avaliando seu
-            histórico de vida e saúde para criar um plano de recuperação mais
-            eficaz e humano.
+            Em momentos de crise, quando o diálogo não é mais possível, contamos
+            com equipes parceiras treinadas para uma abordagem segura, rápida e
+            humanizada, garantindo a integridade e o respeito ao paciente
+            durante o transporte até a clínica.
           </p>
 
           <Link
