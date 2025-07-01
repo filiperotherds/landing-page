ffcdf338aff7299b2081242b4dc3e88c91815b62
Filipe <filiperotherds@gmail.com>
2025-07-01 13:45:11 -0300

    resize font at feture description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -295,7 +295,7 @@ export default function Home() {
             Tratamento Individualizado
           </h1>
 
-          <p className="text-xs text-zinc-700 text-center">
+          <p className="text-[13px] text-zinc-700 text-center">
             Entendemos que cada pessoa é única. Por isso, o tratamento é 100%
             focado nas necessidades específicas do paciente, avaliando seu
             histórico de vida e saúde para criar um plano de recuperação mais
@@ -326,7 +326,7 @@ export default function Home() {
             Apoio Familiar
           </h1>
 
-          <p className="text-xs text-zinc-700 text-center">
+          <p className="text-[13px] text-zinc-700 text-center">
             A dependência química afeta toda a família. Oferecemos sessões de
             terapia e grupos de apoio para os familiares, fornecendo as
             ferramentas necessárias para entender o processo, curar as relações
@@ -357,7 +357,7 @@ export default function Home() {
             Suporte para Remoção 24h
           </h1>
 
-          <p className="text-xs text-zinc-700 text-center">
+          <p className="text-[13px] text-zinc-700 text-center">
             Em momentos de crise, quando o diálogo não é mais possível, contamos
             com equipes parceiras treinadas para uma abordagem segura, rápida e
             humanizada, garantindo a integridade e o respeito ao paciente
@@ -376,7 +376,7 @@ export default function Home() {
           </Link>
         </div>
       </section>
-      
+
       {/* Benefits Section */}
       <section>benefícios section</section>
       {/* FAQ Section */}
