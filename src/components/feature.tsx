ac3130d0b336c37b6e8a617d008969e36e2f0c44
Filipe <filiperotherds@gmail.com>
2025-07-12 18:59:11 -0300

    remove unused component

--- src/components/feature.tsx
+++ src/components/feature.tsx
@@ -1,6 +1,4 @@
 import { DeliveryTruck } from "iconoir-react";
-import { ArrowUpRight } from "lucide-react";
-import Link from "next/link";
 import Image from "next/image";
 
 export default function Feature() {
